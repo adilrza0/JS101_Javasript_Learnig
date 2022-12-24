0a0549c61e78ae03f78c68f5766ce69529f1fa57
= <=>
2022-12-24 20:50:07 +0530

    varieabldatatype

---
+++ Variable & Data type/problem4.js
@@ -0,0 +1,11 @@
+let name ="adil";
+let rollno = "21";
+let grade = "12th";
+let section= "c";
+let school = "STRPS"
+let Marks = [
+  "chemistry -   82","Biology  -  85", "Phsyics   -    78"
+]
+console.log(name,rollno,grade,section,school)
+console.log("-----------------------------------------")
+console.log(Marks)
